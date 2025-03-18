e504067b83b3d0590ebdf355e85ff3e827a4c3e7
GUGAN VS <guganvs2113@gmail.com>
2025-03-18 11:19:27 +0530

    Update settings.py

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -123,15 +123,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = ['api.backends.HospitalAuthBackend']
-
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles')
-    ]
 
 TIME_ZONE = 'Asia/Kolkata'
 
