311ac7859427171e3fe5ffbf52db22ebbc702ffb
GUGAN VS <guganvs2113@gmail.com>
2025-04-15 16:04:19 +0530

    Update settings.py

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -87,7 +87,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
     'default': dj_database_url.config(
         default=DATABASE_URL,
-        # engine='django.db.backends.postgresql',# Ensure the engine is set
+        engine='django.db.backends.postgresql',# Ensure the engine is set
         conn_max_age=600,
         ssl_require=False,
     )
